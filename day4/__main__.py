2d44707475f8c3301b1cf791131b98c4e54a550b
M4RC0Sx <marcos3as3@gmail.com>
2022-12-04 22:18:50 +0100

    feat(day4-a): main file

---
+++ day4/__main__.py
@@ -0,0 +1,24 @@
+#
+# AoC 2022
+# M4RC0Sx - https://github.com/M4RC0Sx
+#
+
+
+from day4 import a, b, common
+
+
+def main():
+
+    input_lines = common.read_input('day4/input.txt')
+
+    a_solution = a.get_solution(input_lines)
+    print('Puzzle A solution:')
+    print(f'\t-> Pairs in one range fully contains each: {a_solution}')
+    # print()
+    # print('Puzzle B solution:')
+    # b_solution = b.get_solution(input_lines, char_priorities)
+    # print(f'\t-> Group items priority sum: {b_solution}')
+
+
+if __name__ == '__main__':
+    main()
